4d2b979373ff333560ec7edbc889daaf82461ad3
Oleksandr Isaiev <oleksandr.isaiev@thetrainline.com>
2024-03-23 17:25:52 +0200

    init project

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Oleksandr Isaiev on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
