a260daeec87095ed5791299ce2220efd8bb22542
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-03-23 18:40:37 +0200

    base design for main screen

--- BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -9,10 +9,25 @@ import SwiftUI
 
 struct BarcodeScannerView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+
+                Spacer()
+                    .frame(height: 60)
+
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
         }
-        .padding()
     }
 }
 
